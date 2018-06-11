b6a450417bf8f3cb7d8b2910659959fd6a27bfb2
Yoon Wai Yan <waiyanyoon@gmail.com>
2018-06-11 22:27:32 +0800

    Add page spacing

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,43 @@
 <template>
-  <div>
-    <h2>Expenses</h2>
-  <el-table
-    :data="tableData"
-    stripe
-    style="width: 100%">
-    <el-table-column
-      prop="date"
-      label="日期"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址">
-    </el-table-column>
-  </el-table>
+  <el-container class="main-container">
+    <el-main>
+      <h2>Expenses</h2>
+      <el-table
+        :data="tableData"
+        stripe>
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
 
 
-  <h2>Add New Expense</h2>
-  <el-form ref="formLabelAlign" label-position="left" label-width="100px" :model="formLabelAlign">
-    <el-form-item label="Name">
-      <el-input v-model="formLabelAlign.name"></el-input>
-    </el-form-item>
-    <el-form-item label="Activity zone">
-      <el-input v-model="formLabelAlign.region"></el-input>
-    </el-form-item>
-    <el-form-item label="Activity form">
-      <el-input v-model="formLabelAlign.type"></el-input>
-    </el-form-item>
-  </el-form>
-  <el-button type="primary" @click="submitForm('formLabelAlign')">Create</el-button>
-  <el-button @click="resetForm('formLabelAlign')">Reset</el-button>
-</div>
+      <h2>Add New Expense</h2>
+      <el-form ref="formLabelAlign" label-position="left" label-width="100px" :model="formLabelAlign">
+        <el-form-item label="Name">
+          <el-input v-model="formLabelAlign.name"></el-input>
+        </el-form-item>
+        <el-form-item label="Activity zone">
+          <el-input v-model="formLabelAlign.region"></el-input>
+        </el-form-item>
+        <el-form-item label="Activity form">
+          <el-input v-model="formLabelAlign.type"></el-input>
+        </el-form-item>
+      </el-form>
+      <el-button type="primary" @click="submitForm('formLabelAlign')">Create</el-button>
+      <el-button @click="resetForm('formLabelAlign')">Reset</el-button>
+    </el-main>
+</el-container>
 </template>
 
 <script>
@@ -90,5 +91,10 @@
   body {
     font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
     margin: 0;
+  }
+
+  .main-container {
+    width: 80%;
+    margin: 0 auto;
   }
 </style>
