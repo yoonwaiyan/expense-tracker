e9abe70eacb0acab0d1d1a213840c251d5526ff6
Wai Yan Yoon <waiyanyoon@gmail.com>
2018-06-30 14:19:15 +0800

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
   <el-container class="main-container">
+    <el-header style="line-height: 60px;">
+      <span class="brand-logo">Expense Tracker</span>
+      <span class="logged-in-as" v-if="currentUser">Logged in as {{ currentUser.email }}</span>
+    </el-header>
     <el-main>
       <router-view></router-view>
     </el-main>
   </el-container>
 </template>
 
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['currentUser'])
+  }
+};
+</script>
+
+
 <style>
 body {
   font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
     'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
   margin: 0;
+}
+
+.brand-logo {
+  font-weight: bold;
+}
+
+.logged-in-as {
+  float: right;
 }
 
 .main-container {
--- src/main.js
+++ src/main.js
@@ -19,6 +19,7 @@ const router = new VueRouter({
 
 new Vue({
   router,
+  store,
   created() {
     Firebase.auth().onAuthStateChanged(user => {
       if (user) {
